db3aca23a0b0b1de7ddf0f67b74e87f8917a9b02
AlexRF14 <alejandro.rodriguezferrer@usp.ceu.es>
2024-10-26 20:33:21 +0200

    eiliminamos fuego aliado

--- Prácticas/Práctica 1/ajedrez.pl
+++ Prácticas/Práctica 1/ajedrez.pl
@@ -1,13 +1,4 @@
-/*
-tablero([a8 , b8 , c8 , d8 , e8 , f8 , g8 , h8],
-        [a7 , b7 , c7 , d7 , e7 , f7 , g7 , h7],
-        [a6 , b6 , c6 , d6 , e6 , f6 , g6 , h6],
-        [a5 , b5 , c5 , d5 , e5 , f5 , g5 , h5],
-        [a4 , b4 , c4 , d4 , e4 , f4 , g4 , h4],
-        [a3 , b3 , c3 , d3 , e3 , f3 , g3 , h3],
-        [a2 , b2 , c2 , d2 , e2 , f2 , g2 , h2],
-        [a1 , b1 , c1 , d1 , e1 , f1 , g1 , h1]).
-*/
+
 % Peones blancos
 casilla(peonBlanco , 1 , 2).
 casilla(peonBlanco , 2 , 2).
@@ -63,7 +54,7 @@ movi(peonBlanco , Col , Fil):-
         not (casilla(_, Col, prox_fil)),
         assert(casilla(peonBlanco , Col , prox_fil)),
         retract(casilla(peonBlanco , Col , Fil)).
-        
+
 movi(peonBlanco , Col , Fil):-
         (prox_fil is Fil + 1, prox_col is + Col + 1;
          prox_fil is Fil + 1, prox_col is + Col - 1),
@@ -71,7 +62,9 @@ movi(peonBlanco , Col , Fil):-
         Col >= 1, Col =< 8,
         casilla(peonBlanco , Col , Fil),
         casilla(_, prox_col , prox_fil),
+        not casilla((peonBlanco, torreBlanco , alfilBlanco , reyBlanco ,  caballoBlanco , reinaBlanco) ,  prox_col , prox_fil),
         assert(casilla(peonBlanco, prox_col , prox_fil)),
+        retract(casilla(_, prox_col , prox_fil)).
         retract(casilla(peonBlanco , Col , Fil)).
 
 movi(peonNegro , Col , Fil):-
